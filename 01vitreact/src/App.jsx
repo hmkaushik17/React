1d81447d83a99a34ea9676f88025a97acf09eb84
Himanshi <hmkaushik17@gmail.com>
2026-03-07 14:15:04 +0530

    Added basic react and vite react folders

---
+++ 01vitreact/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from './chai'; 
+function App() {
+  return (
+     <>
+       <h1>Namaste React with Vite || Himanshi</h1>
+      <Chai></Chai>
+     </>
+  
+    
+    
+  )
+}
+
+export default App
---
+++ 01vitreact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai()
+    {
+        return(<h2>React with chai</h2>)
+    }
+
+    export default Chai;
+    
