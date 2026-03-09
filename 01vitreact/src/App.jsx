3960cad1b7667883fd22abdb78bde8a23f221323
Himanshi <hmkaushik17@gmail.com>
2026-03-09 12:48:38 +0530

    Custom React

--- 01vitreact/src/App.jsx
+++ 01vitreact/src/App.jsx
@@ -1,14 +1,12 @@
-import Chai from './chai'; 
-function App() {
-  return (
-     <>
-       <h1>Namaste React with Vite || Himanshi</h1>
-      <Chai></Chai>
-     </>
-  
-    
-    
+import Chai from "./chai"
+
+function App()
+{ 
+  const username =" Himanshi"
+  return(<>
+    <h1>Hii {username} </h1>
+    <Chai></Chai>
+  </>
   )
 }
-
 export default App
